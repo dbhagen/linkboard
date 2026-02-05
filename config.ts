1bb6b19e79372a958be2fc7d070302022858203c
Daniel Hagen <5039466+dbhagen@users.noreply.github.com>
2026-02-04 23:52:03 -0600

    chore: fix links and icons

--- config.ts
+++ config.ts
@@ -27,18 +27,18 @@ const data: Data = {
     },
     {
       name: "Baby List Registry",
-      url: "https://twitter.com/hangerthem",
-      icon: Gift,
+      url: "https://www.babylist.com/list/kirsten-daniel-hagen",
+      icon: JournalAlbum,
     },
     {
       name: "Amazon Registry",
-      url: "https://github.com/hangerthem",
-      icon: Gift,
+      url: "https://www.target.com/gift-registry/gift/kirsten-and-daniel-1baby",
+      icon: JournalAlbum,
     },
     {
       name: "Target Registry",
       url: "https://www.linkedin.com/in/franti%C5%A1ek-borisjuk-022686225/",
-      icon: Gift,
+      icon: JournalAlbum,
     },
   ],
   sortByLength: true,
